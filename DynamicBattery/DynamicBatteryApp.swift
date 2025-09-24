0d2f9a4e997414029f163fe8c4c12aba822fcfcc
Tau Leung <tau@Taus-MacBook-Air.local>
2025-09-24 13:08:11 +0800

    init proj

---
+++ DynamicBattery/DynamicBatteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicBatteryApp.swift
+//  DynamicBattery
+//
+//  Created by Tau Leung on 24/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct DynamicBatteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DynamicBatteryTests/DynamicBatteryTests.swift
@@ -0,0 +1,16 @@
+//
+//  DynamicBatteryTests.swift
+//  DynamicBatteryTests
+//
+//  Created by Tau Leung on 24/09/2025.
+//
+
+import Testing
+
+struct DynamicBatteryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
